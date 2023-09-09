3368ea5e2568f3b5433d4189cce868bd0b4c462d
Osamuyi <muyixone@yahoo.com>
2023-09-09 03:57:00 +0100

    Create a 404 response

--- app.js
+++ app.js
@@ -8,6 +8,14 @@ const app = express();
 
 // Middlewares
 app.use(express.json());
+app.use('/hng/task1/api', getTrackRoute);
+// app.use('/hng/task1/api', createTrack);
+
+app.use('*', (req, res) => {
+  res.status(404).json({
+    message: 'Resource not found',
+  });
+});
 
 // // error handler
 // app.use(function (err, req, res, next) {
